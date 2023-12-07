3d39152a145c20ffdd9cf5f0ac8ec6ebf15af5fa
Joe Strickland <joseph.a.strickland@gmail.com>
2023-12-07 15:51:44 -0500

    initial commit

---
+++ cybr/version.go
@@ -0,0 +1,8 @@
+// Package aws provides core functionality for making requests to CYBR services.
+package cybr
+
+// SDKName is the name of this AWS SDK
+const SDKName = "cybr-sdk-go"
+
+// SDKVersion is the version of this SDK
+const SDKVersion = goModuleVersion
